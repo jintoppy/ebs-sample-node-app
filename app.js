5da50c4a26948462cf31c2f31fe2da755be63cb1
Jinto Jose <jintoj@thoughtworks.com>
2018-11-20 18:29:21 +0530

    adding testing

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World and this changes by git!'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+module.exports = app;
